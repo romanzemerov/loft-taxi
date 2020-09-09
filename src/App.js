1ac6af0882b030136015a6ddbb44e94c5b343f73
romanzemerov <romanzemerov@gmail.com>
2020-09-09 20:03:12 +0300

    feat: add simple routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import React, { Component } from "react";
 import { Header } from "components/Header";
+import { Map } from "components/Map";
+import { Profile } from "components/Profile";
+import { Login } from "components/Login";
 import "./App.css";
 
 const NAVIGATION = [
   { label: "Карта", href: "map", isActive: true },
-  { label: "Профиль", href: "#", isActive: false },
+  { label: "Профиль", href: "profile", isActive: false },
   { label: "Логин", href: "login", isActive: false },
 ];
 
+const LINK_TO_COMPONENT = {
+  map: <Map />,
+  profile: <Profile />,
+  login: <Login />,
+};
+
 export class App extends Component {
-  state = { navigation: NAVIGATION };
+  state = {
+    navigation: NAVIGATION,
+    pathname: window.location.pathname,
+  };
+
+  getShowingComponent = () => {
+    const { pathname } = this.state;
+
+    return LINK_TO_COMPONENT[pathname.slice(1)] || <Map />;
+  };
+
+  handleChangePathname = (e) => {
+    e.preventDefault();
+    const pathname = e.target.pathname;
+
+    this.setState({ pathname });
+
+    this.setState(({ navigation }) => {
+      const href = pathname.slice(1);
+
+      const newNavigation = navigation.reduce((acc, item) => {
+        if (item.href === href) {
+          item.isActive = true;
+        } else {
+          item.isActive = false;
+        }
+
+        return [...acc, item];
+      }, []);
+
+      return { navigation: newNavigation };
+    });
+  };
+
+  componentDidUpdate() {
+    const { pathname } = this.state;
+    window.history.pushState({}, null, pathname);
+  }
 
   render() {
     const { navigation } = this.state;
+    const component = this.getShowingComponent();
 
-    return <Header navItems={navigation} />;
+    return (
+      <>
+        <Header navItems={navigation} handleClick={this.handleChangePathname} />
+        {component}
+      </>
+    );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-export const Header = ({ navItems }) => {
+export const Header = ({ navItems, handleClick }) => {
   return (
     <header>
       Loft Taxi
       <nav>
         <ul>
-          {navItems.map(({ label, href }) => (
+          {navItems.map(({ label, href, isActive }) => (
             <li key={label}>
-              <a href={href}>{label}</a>
+              <a href={isActive ? null : href} onClick={handleClick}>
+                {label}
+              </a>
             </li>
           ))}
         </ul>
