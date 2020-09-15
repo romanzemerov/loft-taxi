b5dd7c684e6bc48423f0a4ae24fca9efed7ee8ce
romanzemerov <romanzemerov@gmail.com>
2020-09-15 15:03:20 +0300

    feat: add material-ui and loft theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { theme } from 'loft-taxi-mui-theme';
+import { ThemeProvider } from '@material-ui/core/styles';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
