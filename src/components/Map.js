3c6f57ba6ae236d4589027ed4d5b4dd9ca98fe3e
romanzemerov <romanzemerov@gmail.com>
2020-09-16 08:41:39 +0300

    feat: add mapbox

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  map: {
+    minHeight: 'calc(100vh - 68px)',
+  },
+});
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoicm9tYW56ZW1lcm92IiwiYSI6ImNrZjRlcGdhcDBjY3IyeHA5Mzl3aHk4NncifQ.CVAivYa4dl9DMVGJUoqMTg';
 
 export const Map = () => {
-  return <div>Карта</div>;
+  const classes = useStyles();
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/romanzemerov/ckf4es8aq1l3j19n5iz5mhxkm',
+      center: [36.58, 50.59],
+      zoom: 9,
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div className={classes.map} ref={mapContainerRef}>
+      Карта
+    </div>
+  );
 };
