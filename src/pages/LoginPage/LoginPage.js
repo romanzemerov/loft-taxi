5270f997f89acf7f3f394e787d4a37e116438ca4
romanzemerov <romanzemerov@gmail.com>
2020-09-27 21:12:33 +0300

    refactor: move login and signup components to their pages

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import Login from 'components/Login';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getIsLoading } from 'redux/auth/reducers';
+import { loginRequest } from 'redux/auth/actions';
+import {
+  Button,
+  Link as MaterialLink,
+  Paper,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Logo } from 'loft-taxi-mui-theme';
 import backgroundImage from 'assets/background.jpg';
 
 const useStyles = makeStyles({
+  form: {
+    maxWidth: '500px',
+    padding: '44px 60px',
+  },
+  header: {
+    marginBottom: '30px',
+  },
+  subHeader: {
+    marginBottom: '40px',
+  },
+  input: {
+    marginBottom: '32px',
+    '&:last-of-type': {
+      marginBottom: '0',
+    },
+  },
+  button: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginTop: '40px',
+  },
   loginPage: {
     display: 'flex',
     justifyContent: 'center',
@@ -17,17 +48,102 @@ const useStyles = makeStyles({
   },
 });
 
-const LoginPage = () => {
-  const { loginPage, formWrapper } = useStyles();
+const LoginPage = ({ isLoading, loginRequest }) => {
+  const [{ email, password }, setUser] = useState({
+    email: 'testtestovich5@test.test',
+    password: 'testtest',
+  });
+
+  const {
+    loginPage,
+    formWrapper,
+    form,
+    header,
+    subHeader,
+    input,
+    button,
+  } = useStyles();
+
+  const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    setUser((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginRequest({ email, password });
+  };
 
   return (
     <div className={loginPage}>
       <Logo animated={true} white={true} />
       <div className={formWrapper}>
-        <Login />
+        <Paper className={form}>
+          <Typography className={header} variant="h4">
+            Логин
+          </Typography>
+          <Typography className={subHeader} variant={'body1'}>
+            Новый пользователь?
+            <MaterialLink to="/signup" component={Link} data-testid={'link'}>
+              &nbsp;Зарегистрируйтесь
+            </MaterialLink>
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              className={input}
+              type="email"
+              name="email"
+              id="email"
+              label="Имя пользователя"
+              fullWidth={true}
+              value={email}
+              onChange={handleInputChange}
+              required={true}
+              inputProps={{
+                'data-testid': 'input-email',
+              }}
+            />
+
+            <TextField
+              className={input}
+              type="password"
+              name="password"
+              id="password"
+              label="Пароль"
+              fullWidth={true}
+              value={password}
+              onChange={handleInputChange}
+              required={true}
+              inputProps={{
+                'data-testid': 'input-password',
+              }}
+            />
+            <Button
+              className={button}
+              type={'submit'}
+              variant={'contained'}
+              color={'primary'}
+              data-testid={'login-button'}
+              disabled={isLoading}
+            >
+              Войти
+            </Button>
+          </form>
+        </Paper>
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+const mapStateToProps = (state) => ({
+  isLoading: getIsLoading(state),
+});
+
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import Signup from 'components/Signup';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getIsLoading } from 'redux/auth/reducers';
+import { registerRequest } from 'redux/auth/actions';
+import {
+  Button,
+  Link as MaterialLink,
+  Paper,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Logo } from 'loft-taxi-mui-theme';
 import backgroundImage from 'assets/background.jpg';
@@ -15,19 +25,163 @@ const useStyles = makeStyles({
   formWrapper: {
     marginLeft: '214px',
   },
+  form: {
+    maxWidth: '500px',
+    padding: '44px 60px',
+  },
+  header: {
+    marginBottom: '30px',
+  },
+  subHeader: {
+    marginBottom: '40px',
+  },
+  section: {
+    display: 'flex',
+    '& > *': {
+      marginRight: '16px',
+    },
+  },
+  input: {
+    marginBottom: '32px',
+    '&:last-of-type': {
+      marginBottom: '0',
+    },
+  },
+  button: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginTop: '40px',
+  },
 });
 
-const SignupPage = () => {
-  const { signupPage, formWrapper } = useStyles();
+const SignupPage = ({ isLoading, registerRequest }) => {
+  const [{ email, password, name, surname }, setUser] = useState({
+    email: '',
+    password: '',
+    name: '',
+    surname: '',
+  });
+
+  const {
+    signupPage,
+    formWrapper,
+    form,
+    header,
+    subHeader,
+    section,
+    input,
+    button,
+  } = useStyles();
+
+  const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    setUser((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    registerRequest({ email, password, name, surname });
+  };
 
   return (
     <div className={signupPage}>
       <Logo animated={true} white={true} />
       <div className={formWrapper}>
-        <Signup />
+        <Paper className={form}>
+          <Typography className={header} variant="h4">
+            Регистрация
+          </Typography>
+          <Typography className={subHeader} variant={'body1'}>
+            <MaterialLink to="/login" component={Link} data-testid={'link'}>
+              &nbsp;Уже зарегистрированы?
+            </MaterialLink>
+          </Typography>
+
+          <form onSubmit={handleSubmit}>
+            <TextField
+              className={input}
+              type="email"
+              name="email"
+              id="email"
+              label="Адрес электронной почты"
+              fullWidth={true}
+              value={email}
+              onChange={handleInputChange}
+              required={true}
+              inputProps={{
+                'data-testid': 'input-email',
+              }}
+            />
+            <div className={section}>
+              <TextField
+                className={input}
+                type="text"
+                name="name"
+                id="name"
+                label="Имя"
+                fullWidth={true}
+                value={name}
+                onChange={handleInputChange}
+                required={true}
+                inputProps={{
+                  'data-testid': 'input-name',
+                }}
+              />
+              <TextField
+                className={input}
+                type="text"
+                name="surname"
+                id="surname"
+                label="Фамилия"
+                fullWidth={true}
+                value={surname}
+                onChange={handleInputChange}
+                required={true}
+                inputProps={{
+                  'data-testid': 'input-surname',
+                }}
+              />
+            </div>
+            <TextField
+              className={input}
+              type="password"
+              name="password"
+              id="password"
+              label="Пароль"
+              fullWidth={true}
+              value={password}
+              onChange={handleInputChange}
+              required={true}
+              inputProps={{
+                'data-testid': 'input-password',
+              }}
+            />
+            <Button
+              className={button}
+              type={'submit'}
+              variant={'contained'}
+              color={'primary'}
+              data-testid={'signup-button'}
+              disabled={isLoading}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </Paper>
       </div>
     </div>
   );
 };
 
-export default SignupPage;
+const mapStateToProps = (state) => ({
+  isLoading: getIsLoading(state),
+});
+
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
