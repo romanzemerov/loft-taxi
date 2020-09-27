301b4d46148d1b469271eea143a066b0ec552df9
romanzemerov <romanzemerov@gmail.com>
2020-09-27 16:06:19 +0300

    refactor: remove unused method

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { loginRequest } from 'redux/auth/actions';
 import { connect } from 'react-redux';
 
 const PrivateRoute = ({ component: Component, path, isLoggedIn, ...rest }) => {
@@ -17,8 +16,4 @@ const PrivateRoute = ({ component: Component, path, isLoggedIn, ...rest }) => {
 
 const mapStateToProps = (state) => ({ isLoggedIn: state.auth.isLoggedIn });
 
-const mapDispatchToProps = {
-  loginRequest,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PrivateRoute);
+export default connect(mapStateToProps, null)(PrivateRoute);
